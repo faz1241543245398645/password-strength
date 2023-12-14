da7a9394c1d1a09e84b2c50205474b3c488abf44
faz1241543245398645 <111303147+faz1241543245398645@users.noreply.github.com>
2023-12-13 22:11:23 -0800

    Push Prortflio

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <PasswordField/>
+  </div>
+</template>
+
+<script>
+  import PasswordField from './components/PasswordField.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      PasswordField
+    }
+  }
+</script>
+
+<style lang="scss">
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(to bottom left, #04AFA8, #2F3A45);
+  }
+
+  #app {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 1.8rem;
+    color: #333;
+  }
+</style>
